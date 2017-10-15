6cdae5d42b2b513cb4eed47682fee32ffac6db28
FUJINOWIN\takuro <phy.l.050492@gmail.com>
2017-10-15 23:25:05 +0900

    change some print contents

--- test/test1.py
+++ test/test1.py
@@ -13,16 +13,20 @@ functions = []
 def test1_func():#values, functions):
     values = []
     functions = []
-    with open("/Volumes/SDHC/takuro/Documents/sample_codes/tags","r") as f:
+    #with open("/Volumes/SDHC/takuro/Documents/sample_codes/tags","r") as f:
+    with open("S:/takuro/Documents/sample_codes/tags", "r" ) as f:
         for line in f:
             line = line.replace("\n","")
             L = line.split('\t')
+            print L
+            """
             if (len(L) > 3) and (L[3] == "v"):
                 print L[0]
                 values.append(L[0])
             elif (len(L) > 3) and (L[3] == "f"):
                 print L[0]
                 functions.append(L[0])
+            """
 
     #print values
     #print functions
@@ -30,5 +34,5 @@ def test1_func():#values, functions):
     return values
 
 #print values, functions
-#test1_func(values, functions)
+test1_func()    #values, functions)
 #print values, functions
